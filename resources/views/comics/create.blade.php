3502e32b8690b1a3385fce9679a3a63c161dcfb2
mdeias <mdeias@icloud.com>
2022-01-31 19:28:40 +0100

    errore che appare in ogni singolo campo

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -22,32 +22,62 @@
             <div class="mb-3">
               <label for="title" class="form-label">Titolo</label>
 
-              <input type="text" class="form-control" id="title" name="title" placeholder="Titolo" >
+              @error('title')
+              <p>{{$message}}</p>                 
+              @enderror
+
+              <input type="text" class="form-control @error('title')
+              is-invalid  
+              @enderror" id="title" name="title" placeholder="Titolo" >
+              
             </div>
 
             <div class="mb-3">
                 <label for="description" class="form-label">Descrizione</label>
-  
-                <textarea  class="form-control" id="description" name="description" placeholder="Descrizione" >
+                @error('description')
+                    <p>{{$message}}</p>                 
+                @enderror
+                <textarea  class="form-control 
+                @error('description')
+                is-invalid  
+                @enderror" id="description" name="description" placeholder="Descrizione" >
                 </textarea>
             </div>
 
             <div class="mb-3">
                 <label for="thumb" class="form-label">Link della copertina</label>
-  
-                <input type="text" class="form-control" id="thumb" name="thumb" placeholder="Link" >
+                @error('thumb')
+                     <p>{{$message}}</p>                 
+                @enderror
+                <input type="text" class="form-control
+                @error('thumb')
+                is-invalid  
+                @enderror"
+                id="thumb" name="thumb" placeholder="Link" >
             </div>
 
             <div class="mb-3">
                 <label for="price" class="form-label">Prezzo</label>
-  
-                <input type="number" class="form-control" id="price" name="price" placeholder="Prezzo" >
+                @error('price')
+                    <p>{{$message}}</p>                 
+                @enderror
+                <input type="number" class="form-control
+                @error('price')
+                is-invalid  
+                @enderror" 
+                id="price" name="price" placeholder="Prezzo" >
             </div>
 
             <div class="mb-3">
                 <label for="series" class="form-label">Serie</label>
-  
-                <input type="text" class="form-control" id="series" name="series" placeholder="Serie" >
+                @error('series')
+                    <p>{{$message}}</p>                 
+                @enderror
+                <input type="text" class="form-control
+                @error('series')
+                is-invalid  
+                @enderror"
+               id="series" name="series" placeholder="Serie" >
             </div>
 
             <div class="mb-3">
@@ -58,8 +88,14 @@
 
             <div class="mb-3">
                 <label for="type" class="form-label">Tipo</label>
-  
-                <input type="text" class="form-control" id="type" name="type" placeholder="Tipo" >
+                @error('type')
+                    <p>{{$message}}</p>                 
+                @enderror
+                <input type="text" class="form-control
+                @error('type')
+                is-invalid  
+                @enderror"
+               id="type" name="type" placeholder="Tipo" >
             </div>
 
             <button class="btn btn-primary" type="submit">
