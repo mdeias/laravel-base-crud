7cce89652e1a033611d267cffde92390458cfd9a
mdeias <mdeias@icloud.com>
2022-01-31 22:59:57 +0100

    controlli form edit

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -57,6 +57,7 @@ class ComicController extends Controller
                 'description.max'=>'Descrizione troppo lunga',
                 'description.min'=>'Descrizione troppo corta',
 
+                'thumb.required'=>'Url è un campo obbligatorio',
                 'thumb.max'=>'Url troppo lungo',
 
                 'price.required'=>'Il prezzo è un campo obbligatorio',
@@ -68,6 +69,7 @@ class ComicController extends Controller
                 'series.min'=>'Serie troppo lungo',
 
                 
+                'type.required'=>'Il tipo è un campo obbligatorio',
                 'type.max'=>'Tipo troppo lungo',
                 'type.min'=>'Tipo troppo lungo',
 
@@ -124,6 +126,46 @@ class ComicController extends Controller
      */
     public function update(Request $request, Comic $comic)
     {
+
+
+        $request->validate(
+            [
+                'title'=>'required|max:50|min:2',
+                'description'=>'required|min:10',
+                'thumb'=>'required|max:250',
+                'price'=>'required|numeric|min:1|max:99',
+                'series'=>'required|max:60|min:2',
+                'type'=>'required|max:20|min:2',
+            ],
+            [
+                'title.required'=>'Il titolo è un campo obbligatorio',
+                'title.max'=>'Titolo troppo lungo',
+                'title.min'=>'Titolo troppo corto',
+
+                'description.required'=>'La descrizione è un campo obbligatorio',
+                'description.max'=>'Descrizione troppo lunga',
+                'description.min'=>'Descrizione troppo corta',
+
+                'thumb.required'=>'Url è un campo obbligatorio',
+                'thumb.max'=>'Url troppo lungo',
+
+                'price.required'=>'Il prezzo è un campo obbligatorio',
+                'price.max'=>'Il prezzo è troppo alto',
+                'price.min'=>'Il prezzo è troppo basso',
+
+                'series.required'=>'La serie è obbligatoria',
+                'series.max'=>'Serie troppo lunga',
+                'series.min'=>'Serie troppo lungo',
+
+                
+                'type.required'=>'Il tipo è un campo obbligatorio',
+                'type.max'=>'Tipo troppo lungo',
+                'type.min'=>'Tipo troppo lungo',
+
+            ]
+
+        );
+
         $data = $request->all();
         
         
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -4,6 +4,16 @@
     <div class="container">
         <h1>Modifica: {{$comic->title}}</h1>
 
+        @if ($errors->any())
+      
+            <div class="alert alert-danger" role="alert">
+             <ul>
+                 @foreach ($errors->all() as $error)
+                     <li> {{$error}} </li>
+                 @endforeach
+             </ul>
+            </div>
+        @endif
         <form action="{{ route('comics.update', $comic) }}" method="POST">
             @csrf
             @method('PUT')
@@ -11,45 +21,84 @@
             <div class="mb-3">
               <label for="title" class="form-label">Titolo</label>
 
-              <input type="text" class="form-control" id="title" name="title" value= {{ $comic->title }} placeholder="Titolo" >
+                @error('title')
+                    <p>{{$message}}</p>                 
+                @enderror
+
+              <input type="text" class="form-control @error('title')
+              is-invalid  
+              @enderror" 
+              id="title" name="title" value= {{ old('title'), $comic->title }} placeholder="Titolo" >
             </div>
 
             <div class="mb-3">
                 <label for="description" class="form-label">Descrizione</label>
+
+                @error('description')
+                    <p>{{$message}}</p>                 
+                 @enderror
   
-                <textarea  class="form-control" id="description" name="description" placeholder="Descrizione" >
-                    {{ $comic->description }} 
-                </textarea>
+                <textarea  class="form-control
+                @error('description')
+                is-invalid  
+                @enderror" 
+                id="description" name="description" placeholder="Descrizione" >{{old('description'), $comic->description}}</textarea>
             </div>
 
             <div class="mb-3">
                 <label for="thumb" class="form-label">Link della copertina</label>
-  
-                <input type="text" class="form-control" id="thumb" name="thumb" value= {{ $comic->thumb }} placeholder="Link" >
+
+                @error('thumb')
+                    <p>{{$message}}</p>                 
+                @enderror
+
+                <input type="text" class="form-control @error('thumb')
+                is-invalid  
+                @enderror" id="thumb" name="thumb" value= {{old('thumb'), $comic->thumb }} placeholder="Link" >
             </div>
 
             <div class="mb-3">
                 <label for="price" class="form-label">Prezzo</label>
-  
-                <input type="number" class="form-control" id="price" name="price" value= {{ $comic->price }} placeholder="Prezzo" >
+
+                @error('price')
+                     <p>{{$message}}</p>                 
+                @enderror
+
+                <input type="number" class="form-control @error('price')
+                is-invalid  
+                @enderror"
+                 id="price" name="price" value= {{ old('price'), $comic->price }} placeholder="Prezzo" >
             </div>
 
             <div class="mb-3">
                 <label for="series" class="form-label">Serie</label>
-  
-                <input type="text" class="form-control" id="series" name="series" value= {{ $comic->series }} placeholder="Serie" >
+
+                @error('series')
+                    <p>{{$message}}</p>                 
+                @enderror
+                <input type="text" class="form-control
+                @error('series')
+                is-invalid  
+                @enderror" id="series" name="series" value= {{old('series'), $comic->series }} placeholder="Serie" >
             </div>
 
             <div class="mb-3">
                 <label for="sale_date" class="form-label">Data di uscita</label>
-  
+
                 <input type="date" class="form-control" id="sale_date" name="sale_date" value= {{ $comic->sale_date }} placeholder="Data" >
             </div>
 
             <div class="mb-3">
                 <label for="type" class="form-label">Tipo</label>
-  
-                <input type="text" class="form-control" id="type" name="type" value= {{ $comic->type }} placeholder="Tipo" >
+
+                @error('type')
+                    <p>{{$message}}</p>                 
+                @enderror
+
+                <input type="text" class="form-control
+                @error('type')
+                is-invalid  
+                @enderror" id="type" name="type" value= {{old('type') $comic->type }} placeholder="Tipo" >
             </div>
 
             <button class="btn btn-primary" type="submit"> 
